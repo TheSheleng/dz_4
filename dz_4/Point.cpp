27cc2d91ea10bfd949cebb22baa054fd38a4f46c
Shel <sheleng55@gmail.com>
2022-09-07 09:54:30 +0300

    Создал реализацию для методов ввода, вывоа, геттера и сеттера.

--- dz_4/Point.cpp
+++ dz_4/Point.cpp
@@ -1,1 +1,32 @@
 #include "Point.h"
+
+void Point::enter()
+{
+    cout << "Enter coords: " << endl;
+    for (double& i : coords)
+    {
+        cout << "Axis coord: ";
+        cin >> i;
+    }
+
+    cout << endl;
+}
+
+void Point::print()
+{
+    cout << "Point: ";
+    for (const double i : coords)
+        cout << i << " ";
+
+    cout << endl;
+}
+
+void Point::setCoord(int a_I, double coord)
+{
+    coords[a_I] = coord;
+}
+
+double Point::getCoord(int a_I)
+{
+    return coords[a_I];
+}
