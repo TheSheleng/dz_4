7fdd198efbc4ee6b6e594875b03863a6fa1be175
Shel <sheleng55@gmail.com>
2022-09-07 09:56:04 +0300

    Создал реализацию для методов сохранения и загрузки.

--- dz_4/Point.cpp
+++ dz_4/Point.cpp
@@ -1,1 +1,19 @@
 #include "Point.h"
+
+void saveFile(const char* path, int index)
+{
+    fstream file(path, ios::binary | ios::out);
+    file.seekp(index * (sizeof * this), ios::beg);
+
+    file.write((char*)&*this, sizeof * this);
+    file.close();
+}
+
+void getFile(const char* path, int index)
+{
+    fstream file(path, ios::binary | ios::in);
+    file.seekp(index * (sizeof * this), ios::beg);
+
+    file.read((char*)&*this, sizeof * this);
+    file.close();
+}
