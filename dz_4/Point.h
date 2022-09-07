bff54836de0d9c89441c0d2da4f29195d0d3fe89
Shel <sheleng55@gmail.com>
2022-09-07 09:46:53 +0300

    Создал мкросы для обозначения количесва осей пространства и индефикатора каждой из осей пространства. А так же в классе Point создал поля coords и поля ввода, вывода, геттер. сеттер, сохранения и загрузки из файла (без реализации).

--- dz_4/Point.h
+++ dz_4/Point.h
@@ -1,7 +1,28 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#define DIMS 3
+
+#define a_X 0
+#define a_Y 1
+#define a_Z 2
+
 class Point
 {
+    double coords[DIMS] = {};
 
+public:
+    void enter();
+    void print();
+
+    void setCoord(int a_I, double coord);
+    double getCoord(int a_I);
+
+    void saveFile(const char* path, int index);
+    void getFile(const char* path, int index);
 };
 
