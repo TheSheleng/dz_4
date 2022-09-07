bce315e35048f9fb67ecbf8563a094207734eb7a
Shel <sheleng55@gmail.com>
2022-09-07 09:50:02 +0300

    В main прописал пример использывания класса Point.

--- dz_4/dz_4.cpp
+++ dz_4/dz_4.cpp
@@ -5,5 +5,16 @@ using namespace std;
 
 int main()
 {
+    const char path[] = "testFile.txt";
 
+    Point test;
+    test.enter();
+    test.saveFile(path, 0);
+
+    Point test2;
+    test2.getFile(path, 0);
+    test2.print();
+
+    test.setCoord(a_X, 0);
+    cout << test.getCoord(a_X) << endl;
 }
